044929eac04d9d3522553bc84af7cbdbfb77bad2
Duhncan <duhncang@gmail.com>
2024-12-04 06:52:44 +1100

    updated leader key in wezterm with updated shhortcuts

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,27 +1,91 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+-- .wezterm.lua
+-- My Wezterm Configuration
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+local wezterm = require 'wezterm'
+local act = wezterm.action
+-- Use config builder if possible
+local config = {}
+if wezterm.config_builder then config = wezterm.config_builder()
+end
+
+-- Shell
+config.default_domain = 'WSL:Ubuntu'
 
 -- Colour Scheme & Font
 config.color_scheme = 'Catppuccin Macchiato'
 config.font = wezterm.font("0xProto Nerd Font", {weight="Regular", stretch="Normal", style="Normal"}) -- C:\USERS\DUHNC\APPDATA\LOCAL\MICROSOFT\WINDOWS\FONTS\0XPROTONERDFONT-REGULAR.TTF, DirectWrite
 
--- Window Configuration 
+-- Window 
 config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = false
+config.window_background_opacity = 0.9
+
+-- Tab
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false 
 config.tab_and_split_indices_are_zero_based = true
-config.window_background_opacity = 0.9
+config.hide_tab_bar_if_only_one_tab = false
 
--- Default Terminal
-config.default_domain = 'WSL:Ubuntu'
-
--- Quick Select 
+-- Key Mappings
+config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 1500 }
 config.keys = {
-    { key = ' ', mods = 'SHIFT|CTRL', action = wezterm.action.QuickSelect },
-  }
+    {
+        mods = "LEADER",
+        key = "c",
+        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },
+    {
+        mods = "LEADER",
+        key = "x",
+        action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+        mods = "LEADER",
+        key = "b",
+        action = wezterm.action.ActivateTabRelative(-1)
+    },
+    {
+        mods = "LEADER",
+        key = "n",
+        action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+        mods = "LEADER",
+        key = "o",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "p",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+    },
+    { 
+        key = ' ', 
+        mods = 'LEADER', 
+        action = wezterm.action.QuickSelect 
+    }, 
+}
+
+-- Leader Key Activity
+wezterm.on("update-right-status", function(window, _)
+  local SOLID_LEFT_ARROW = ""
+  local ARROW_FOREGROUND = { Foreground = { Color = "#c6d0f6" } }
+  local prefix = ""
+
+  if window:leader_is_active() then
+    prefix = " " .. utf8.char(0x1F440)
+    SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  end
+
+  if window:active_tab():tab_id() ~= 0 then
+    ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+  end
+
+  window:set_left_status(wezterm.format {
+    { Background = { Color = "#b7bdf8"} },
+    { Text = prefix },
+    ARROW_FOREGROUND,
+    { Text = SOLID_LEFT_ARROW}
+  })
+end)
 
 return config
